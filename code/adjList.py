caeff6d7fc4835b740359ebf005335a4f81cd60f
NotTimm <gamer.tdogtim@gmail.com>
2023-04-18 01:12:53 -0500

    small adjustment

--- code/adjList.py
+++ code/adjList.py
@@ -1,3 +1,4 @@
+import random
 class Vertice:
     def __init__(self, degree):
         self.degree = degree
@@ -42,11 +43,44 @@ class AdjacencyList:
 
         self.vertices[vert2].edges = edge2
         self.vertices[vert2].degree += 1
+
+    ### Build Scenarios Below ###
+
+    def completeBuild(graph):
+        vertI = 0
+        while vertI < len(graph.vertices)-1:
+            vertO = vertI+1
+            graph.addEdge(vertI, vertO)
+            vertI += 1
+    
+    def cycleBuild(graph):
+        vertI = 0
+        while vertI < len(graph.vertices)-1:
+            graph.addEdge(vertI, vertI+1)
+            vertI += 1
+        graph.addEdge(0, len(graph.vertices)-1)
+
+    def randomUniformBuild(graph, conflicts):
+        if conflicts > len(graph.vertices) * (len(graph.vertices)-1)/2:
+            print("ERROR too many conflicts")
+            exit(420)
+        i = 0
+        while i < conflicts:
+            vert1 = 0
+            vert2 = 0
+
+            while vert1 == vert2 or graph.edgeExists(vert1, vert2):
+                vert1 = random.randint(0, len(graph.vertices)-1)
+                vert2 = random.randint(0, len(graph.vertices)-1)
+            graph.addEdge(vert1, vert2)
+            i += 1
+
+    ### Print Function ###
     
     def printList(self):
         print("Vert #: ", len(self.vertices))
         for index, i in enumerate(self.vertices):
-            print("index:", index, ", degree:", i.degree, " }", sep = "", end ="")
+            print("VertID: ", index, ", Degree: ", i.degree, " }", sep = "", end ="")
             cur = i.edges
             while(cur):
                 print("->", cur.destination, end = " ")
